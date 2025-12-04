51853503e6d8386188b12da7b794caa657a88295
muhammad usman <muhammad.usman@integrationxperts.com>
2025-12-04 11:21:45 +0500

    working

--- src/navigation/MainStack/index.js
+++ src/navigation/MainStack/index.js
@@ -1,6 +1,5 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import Home from '../../Screens/Home';
 import Login from '../../Screens/Login';
